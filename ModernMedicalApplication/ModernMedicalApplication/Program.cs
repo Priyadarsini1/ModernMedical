a97e45288c4083737fa56623829c56f7d84884ca
Priyadarsini1 <priyadarsiniantony@gmail.com>
2014-05-29 15:58:55 -0400

    ModernMedical
    If the number is divisible by 3 only, output Modern
    c. If the number is divisible by 5 only, output Medical
    d. If the number is divisible by both 3 and 5, output ModernMedical
    e. If the number is not divisible by either 3 or 5, only output the
    number itself.
    f. The correct output for the first 20 of the 100 numbers should look
    like:
    
    1
    2
    Modern
    4
    Medical
    Modern
    7
    8
    Modern
    Medical
    11
    Modern
    13
    14
    ModernMedical
    16
    17
    Modern
    19
    Medical

---
+++ ModernMedicalApplication/ModernMedicalApplication/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModernMedicalApplication
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                foreach (var val in Enumerable.Range(1, 100).Select(n => Calculate(n)))
+                    Console.WriteLine(val);
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+        }
+
+        static private string Calculate(int value)
+        {
+            if (value % 15 == 0)
+            {
+                return "ModernMedical";
+            }
+            if (value % 3 == 0)
+            {
+                return "Modern";
+            }
+            if (value % 5 == 0)
+            {
+                return "Medical";
+            }
+            return value.ToString();
+        }
+    }
+}
